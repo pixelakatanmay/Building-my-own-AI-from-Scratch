9d4c47d6974ea7cc87296ea3b2e2396af5189205
Tanmay Sahu <tanmaysahu196@gmail.com>
2026-09-01 01:46:37 +0530

    added two files inside final - learnner.py and neuron.py

---
+++ final/learner.py
@@ -0,0 +1,41 @@
+import random
+
+#traning data
+training_data=[
+(-3,0),
+(-2,0),
+(-1,0),
+(1,1),
+(2,1),
+(3,1),
+]
+
+#random starting values
+weight= random.uniform(-1,1)
+bias= random.uniform(-1,1)
+
+learning_rate=0.1
+
+for epoch in range(100):
+    total_error=0
+
+    for x, correct_answer in training_data:
+        #make a prediction
+        prediction = x*weight+bias
+        #calculate errors
+        error=correct_answer - prediction
+        #adjust the weight and bias
+        weight += learning_rate * error * x
+        bias += learning_rate * error
+
+        total_error += abs(error)
+
+        print(
+            "Epoch: ",
+            epoch,
+            "Error: ",
+            round(total_error,4)
+        )
+
+print("\nFinal weight: ",weight)
+print("Final bias: ",bias)
---
+++ final/neuron.py
@@ -0,0 +1,11 @@
+x1 = 2
+x2 = 3
+
+w1 = -5
+w2 = 7
+
+bias = 0.1
+
+output = (x1 * w1) + (x2 * w2) + bias
+
+print(output)
